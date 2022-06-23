6516687586e2bbba5237e88701eef7f6cf5aba09
Aravind R <ndarvi48332@gmail.com>
2022-06-23 10:30:57 +0530

    session created

--- welcomeapp/views.py
+++ welcomeapp/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate
 from .models import user
 # Create your views here.
 def index(request):
+    if 'username' in request.session:
+        return redirect(home)
+    if request.method =='POST':
+        username = request.POST['username']
+        password = request.POST['pass']
+        user = authenticate(username=username,password=password)
+        if user is not None:
+            request.session['username'] = username
+            return redirect(home)
+        else:
+            return redirect(home)
     return render(request,'index.html')
+
+def home(request):
+    return render(request, 'home.html')
