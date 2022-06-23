12007e58dfb7d4cc87bafe18d962b2835cc5429c
Aravind R <ndarvi48332@gmail.com>
2022-06-23 19:18:21 +0530

    login fixed

--- welcomeapp/views.py
+++ welcomeapp/views.py
@@ -1,21 +1,35 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate
+from django.contrib import messages
 from .models import user
+
+
 # Create your views here.
 def index(request):
     if 'username' in request.session:
         return redirect(home)
-    if request.method =='POST':
+
+    if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['pass']
-        user = authenticate(username=username,password=password)
+        user = authenticate(username=username, password=password)
         if user is not None:
             request.session['username'] = username
             return redirect(home)
         else:
-            return redirect(home)
-    return render(request,'index.html')
+            messages.info(request, 'Error')
+            return redirect(index)
+    else : return render(request, 'index.html')
+
 
 def home(request):
-    return render(request, 'home.html')
+    if 'username' in request.session:
+        # return redirect(index)
+        return render(request, 'home.html')
+
+    else : return redirect(index)
+def user_logout(request):
+
+    request.session.flush()
+    return redirect(index)
