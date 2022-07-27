9e0296e939774a657e476f132dd155ad223bf504
Aravind R <ndarvi48332@gmail.com>
2022-07-27 10:56:53 +0530

    unwanted import issue fixed

--- welcomeapp/views.py
+++ welcomeapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+
 from django.contrib.auth import authenticate
 from django.contrib import messages
 from .models import Homes
